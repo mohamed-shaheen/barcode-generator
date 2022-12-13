daf34e40d787ba3050c19839996b74ed851e8af7
mohamed <hamood_shaheen@outlook.com>
2022-12-13 23:58:09 +0200

    model and barcode function

--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import barcode
+from barcode.writer import ImageWriter
+from io import BytesIO
+from django.core.files import File
 
 # Create your models here.
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    barcode = models.ImageField(upload_to='images/', blank=True)
+    country_id = models.CharField(max_length=1)
+    product_id = models.CharField(max_length=6)
+    factory_id = models.CharField(max_length=6)
+
+
+    def __str__(self):
+        return str(self.name)
+
+    def save(self, *args, **kwargs):
+        EAN = barcode.get_barcode_class('ean13')
+        ean = EAN(f"{self.country_id}{self.product_id}{self.factory_id}", writer=ImageWriter())
+        buffer = BytesIO()
+        ean.write(buffer)
+        self.barcode.save(f"{self.name}_barcode.png", File(buffer), save=False)
+        return super().save(*args, **kwargs)  
